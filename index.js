3425b3e6d9cf0bf1e3f856850aaef5d2befb3131
bukka5sandhya <bukkapatnamsandhya920@gmail.com>
2024-03-19 13:09:40 +0530

    solution

---
+++ index.js
@@ -0,0 +1,6 @@
+//let sectionHomeEle = document.getElementById9("sectionHome");
+//sectionHomeEle.textContent = 'home';
+
+//let sectionLinkedInEle = document.getElementById("sectionLinkedIn");
+//sectionLinkedInEle.textContent = "https://www.linkedin.com/in/sandhya-bukkapatnam-5b8b1b267/";
+
